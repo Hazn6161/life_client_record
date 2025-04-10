f8bfe886dbc8c5cbe141644f35ca2efbfdf810ec
Hazn616 <dinithhgamage@gmail.com>
2025-04-10 09:49:45 +0530

    Office 2025-04-10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   //BASE_URL: 'http://172.21.112.188:8006'
   BASE_URL: 'http://client.cooplife.lk:8006'
-  //BASE_URL: 'http://localhost:8006'
+  //BASE_URL: 'http://localhost:8007'
 };
 
 /*
